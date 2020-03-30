d775be8d0316019d58e1e50d607cfd73c46b6969
Selenam28 <seleyandre@gmail.com>
2020-03-31 00:28:40 +0200

    Initial Commit

---
+++ src/Kassa.java
@@ -0,0 +1,28 @@
+public class Kassa {
+
+    private int aantalProducten;
+    private double totaal;
+
+    public Kassa(){
+
+    }
+
+    public int getAantalProducten(){
+        return 0;
+    }
+
+    public double getTotaal(){
+        return 0.0;
+    }
+
+    public void voegProductToe(){
+
+    }
+
+
+}
+
+
+
+
+
